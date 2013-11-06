3fa3a88332f46a1442b4730026c6d2a1f8e8541d
Tim Burke <tim.burke@gmail.com>
2013-11-05 18:26:50 -0800

    Improve Python completion
    Yeah, using escape for completion matching was a terrible idea; it breaks the arrow keys (likely among other things.) Use tab instead, but insert spaces if we're not in the middle of anything.

--- .python/startup.py
+++ .python/startup.py
@@ -7,13 +7,28 @@ except ImportError:
                 import pyreadline as readline
         # throw open a browser if we fail both readline and pyreadline
         except ImportError:
-                import webbrowser                
+                import webbrowser
                 webbrowser.open("http://ipython.scipy.org/moin/PyReadline/Intro#line-36")
                 # throw open a browser
         #pass
 else:
         import rlcompleter
-        if(sys.platform == 'darwin'):
-                readline.parse_and_bind ("bind ^[ rl_complete")
+        class TabCompleter(rlcompleter.Completer):
+                uses_editline = any(s in readline.__doc__ for s in ['libedit', 'editline'])
+                def complete(self, text, state):
+                        if not text:
+                                readline.insert_text('    ')
+                                if TabCompleter.uses_editline:
+                                        # It won't move the cursor for us, apparently
+                                        sys.stdout.write('\x1b[4C')
+                                readline.redisplay()
+                                sys.stdout.flush()
+                                return None
+                        else:
+                                return rlcompleter.Completer.complete(self, text, state)
+        readline.set_completer(TabCompleter().complete)
+
+        if TabCompleter.uses_editline: # Different binding syntax
+                readline.parse_and_bind("bind ^I rl_complete")
         else:
-                readline.parse_and_bind("esc: complete")
+                readline.parse_and_bind("tab: complete")
