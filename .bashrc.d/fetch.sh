388c8da6226dd024c6b97a13c4dfe6f229eb64e7
Tim Burke <tim.burke@gmail.com>
2015-01-29 13:59:50 -0800

    Don't change the actual working directory on auto-fetch

--- .bashrc.d/fetch.sh
+++ .bashrc.d/fetch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-cd $HOME
-test -n "$(type -t git)" && (
-	bash -c 'git fetch 2> /dev/null'
-)& 2> /dev/null
-disown $!
+(
+	cd "$HOME";
+	test -n "$(type -t git)" && (
+		bash -c 'git fetch 2> /dev/null'
+	)& 2> /dev/null;
+	disown $!
+)
