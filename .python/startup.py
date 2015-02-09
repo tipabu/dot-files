37aa020d121362c959bf6d95350453190c735794
Tim Burke <tim.burke@gmail.com>
2015-02-09 14:05:15 -0800

    Actually use pyreadline if available; flake8-ify

--- .python/startup.py
+++ .python/startup.py
@@ -1,35 +1,40 @@
-#Code  UUID = '9301d536-860d-11de-81c8-0023dfaa9e40'
-import os
-import sys
-try:
-        import readline
-except ImportError:
-        try:
-                import pyreadline as readline
-        # throw open a browser if we fail both readline and pyreadline
-        except ImportError:
-                import webbrowser
-                webbrowser.open("http://ipython.scipy.org/moin/PyReadline/Intro#line-36")
-                # throw open a browser
-        #pass
-else:
-        import rlcompleter
-        class TabCompleter(rlcompleter.Completer):
-                uses_editline = any(s in readline.__doc__ for s in ['libedit', 'editline'])
-                def complete(self, text, state):
-                        if not text:
-                                readline.insert_text('    ')
-                                if TabCompleter.uses_editline:
-                                        # It won't move the cursor for us, apparently
-                                        sys.stdout.write('\x1b[4C')
-                                readline.redisplay()
-                                sys.stdout.flush()
-                                return None
-                        else:
-                                return rlcompleter.Completer.complete(self, text, state)
-        readline.set_completer(TabCompleter().complete)
+import json  # noqa
+import os  # noqa
+import sys  # noqa
 
-        if TabCompleter.uses_editline: # Different binding syntax
-                readline.parse_and_bind("bind ^I rl_complete")
-        else:
-                readline.parse_and_bind("tab: complete")
+readline = None
+try:
+    import readline
+except ImportError:
+    try:
+        import pyreadline as readline
+    # throw open a browser if we fail both readline and pyreadline
+    except ImportError:
+        import webbrowser
+        webbrowser.open("http://ipython.scipy.org"
+                        "/moin/PyReadline/Intro#line-36")
+
+if readline:
+    import rlcompleter
+
+    class TabCompleter(rlcompleter.Completer):
+        uses_editline = any(s in readline.__doc__
+                            for s in ['libedit', 'editline'])
+
+        def complete(self, text, state):
+            if not text:
+                readline.insert_text('    ')
+                if TabCompleter.uses_editline:
+                    # It won't move the cursor for us, apparently
+                    sys.stdout.write('\x1b[4C')
+                readline.redisplay()
+                sys.stdout.flush()
+                return None
+            else:
+                return rlcompleter.Completer.complete(self, text, state)
+    readline.set_completer(TabCompleter().complete)
+
+    if TabCompleter.uses_editline:  # Different binding syntax
+        readline.parse_and_bind("bind ^I rl_complete")
+    else:
+        readline.parse_and_bind("tab: complete")
